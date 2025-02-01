b773776118851b057ecd9624a1463fb30cff3f40
John Christian Cabrera <128894207+rasderty01@users.noreply.github.com>
2025-02-01 21:58:45 +0800

    fix: update Clerk domain to use environment variable for configuration

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://fitting-asp-7.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ],
