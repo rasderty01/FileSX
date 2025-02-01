3a30daff926a4772d5c67f34abd00f82b8e1c836
John Christian Cabrera <128894207+rasderty01@users.noreply.github.com>
2025-02-01 22:29:39 +0800

    chore: update Clerk SDK dependencies and refactor middleware for improved route protection

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs";
+// middleware.ts
 
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/privacy-policy", "/tos", "/about"],
-  // Routes that can always be accessed, and have
-  // no authentication information
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (isProtectedRoute(request)) await auth.protect();
+
+  return NextResponse.next();
 });
 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
