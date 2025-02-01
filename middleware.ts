f4b4e65e189d67b1b753812e36a6bbc08d8594b7
John Christian Cabrera <128894207+rasderty01@users.noreply.github.com>
2025-02-02 01:55:32 +0800

    refactor: streamline CORS handling in middleware by removing redundant headers

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,14 @@
 // middleware.ts
+
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
-  const response = NextResponse.next();
-
-  // Add CORS headers
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS",
-  );
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization",
-  );
-
-  if (request.method === "OPTIONS") {
-    return response;
-  }
-
   if (isProtectedRoute(request)) await auth.protect();
-  return response;
+
+  return NextResponse.next();
 });
 
 export const config = {
